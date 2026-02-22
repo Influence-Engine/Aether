0518754e9e27577733b7f233ed73994e0d787949
Quartzi <34374881+realQuartzi@users.noreply.github.com>
2026-02-22 12:24:04 +0100

    Create Window

--- Aether/Program.cs
+++ Aether/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SDL3;
+
+namespace Aether
+{
+    internal static class Program
+    {
+        const int width = 1280;
+        const int height = 720;
+
+        public static void Main(string[] args)
+        {
+            if(!SDL.Init(SDL.InitFlags.Video))
+            {
+                Console.WriteLine("Init Video Failed...");
+                return;
+            }
+
+            nint window = SDL.CreateWindow("Aether Window", width, height, 0);
+            Console.WriteLine($"Window Created: {window}");
+
+            nint renderer = SDL.CreateRenderer(window, null);
+            Console.WriteLine($"Renderer Created: {renderer}");
+
+            bool running = true;
+
+            while (running)
+            {
+                while(SDL.PollEvent(out SDL.Event e))
+                {
+                    if (e.type == SDL.EventType.Quit)
+                    {
+                        Console.WriteLine("Quit Polled");
+                        running = false;
+                    }
+                }
+
+                SDL.SetRenderDrawColor(renderer, 20, 20, 20, 255);
+                SDL.RenderClear(renderer);
+
+                SDL.RenderPresent(renderer);
+            }
+
+            SDL.DestroyRenderer(renderer);
+            SDL.DestroyWindow(window);
+            SDL.Quit();
+
+            Console.WriteLine("Quit Engine");
+        }
+    }
+}
