573c4ea1e2622a282f722b0b0ad64125b6bb82ba
Quartzi <34374881+realQuartzi@users.noreply.github.com>
2026-02-22 17:25:42 +0100

    Simulation o.o

---
+++ Aether/Simulation/Particle.cs
@@ -0,0 +1,39 @@
+﻿using System.Numerics;
+
+namespace Aether.Simulation
+{
+    public struct Particle
+    {
+        public Vector2 position;
+        public Vector2 velocity;
+
+        public float radius;
+
+        public int type;
+
+        public Particle(Vector2 position, int type, float radius = 1f)
+        {
+            this.position = position;
+            this.velocity = Vector2.Zero;
+            this.radius = radius;
+            this.type = type;
+        }
+
+        public void Update(float deltaTime, float damping, float maxSpeed, int width, int height)
+        {
+            position += velocity * deltaTime;
+            velocity *= damping;
+
+            // Speed limit
+            if (velocity.LengthSquared() > maxSpeed * maxSpeed)
+                velocity = Vector2.Normalize(velocity) * maxSpeed;
+
+            // Wrap around screen
+            if (position.X < 0) position.X = width;
+            if (position.X > width) position.X = 0;
+            if (position.Y < 0) position.Y = height;
+            if (position.Y > height) position.Y = 0;
+
+        }
+    }
+}
