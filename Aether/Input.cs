717e32d62b38ccb6cb82a00cacf2b4779bb06bbc
Quartzi <34374881+realQuartzi@users.noreply.github.com>
2026-02-22 14:45:08 +0100

    is this enough?

---
+++ Aether/Input.cs
@@ -0,0 +1,24 @@
+﻿
+using SDL3;
+using System.Numerics;
+
+namespace Aether
+{
+    public static class Input
+    {
+        static Dictionary<SDL.KeyCode, bool> keyStates = new Dictionary<SDL.KeyCode, bool>();
+        static HashSet<SDL.KeyCode> keysPressedThisFrame = new HashSet<SDL.KeyCode>();
+        static HashSet<SDL.KeyCode> keyReleasedThisFrame = new HashSet<SDL.KeyCode>();
+
+        static bool[] mouseStates = new bool[5];
+        static HashSet<int> mouseButtonPressedThisFrame = new HashSet<int>();
+        static HashSet<int> mouseButtonReleasedThisFrame = new(); // breh
+
+        static Vector2 mousePosition;
+        static Vector2 lastMousePosition;
+        static Vector2 mouseDelta;
+        static Vector2 mouseScrollDelta;
+
+
+    }
+}
